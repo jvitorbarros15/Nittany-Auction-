24e0ddae03b78f506db406485e57d5a1ca26af88
João Vitor Barros <jvitorbarros15@gmail.com>
2026-03-21 14:30:30 -0400

    Add login route on app.py and better styling for the navbar on homepage

--- app.py
+++ app.py
@@ -7,6 +7,19 @@ def homepage():
     return render_template("homepage.html")
 
 # Have to add the route to login page here
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        # We will check DB here
+        print(email, password)
+
+        return "Login attempt"
+
+    return render_template("login.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)

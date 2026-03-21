821a4dce5674e4c7c0d252c8cac3ace849142e03
João Vitor Barros <jvitorbarros15@gmail.com>
2026-03-21 15:06:40 -0400

    Initial commit with foundation of userlogin and comments

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
-
+from flask import Flask, flash, render_template, request, redirect
+import sqlite3
+from werkzeug.security import check_password_hash
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,14 +9,31 @@ def homepage():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    # When user submits the form
     if request.method == "POST":
         email = request.form["email"]
         password = request.form["password"]
 
-        # We will check DB here
-        print(email, password)
+        # Connect to SQLite database
+        connection = sqlite3.connect("nittanyauction.db")
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE email = ?", (email,))  # Look for a user with this email 
+        user = cursor.fetchone()  
+        connection.close()  
 
-        return "Login attempt"
+        # This checks if the password matches the hash stored in the database and gets the role from the database (seller, buyer, helpdesk)
+        if user and check_password_hash(user[2], password):
+            role = user[3].lower()     # This works based on the user tuple format due to using cursor.execute()
+                                       # Example of user tuple: (id, email, password_hash, role)
+            if role == "seller":
+                return redirect("/seller")
+            elif role == "buyer":
+                return redirect("/buyer")
+            elif role == "helpdesk":
+                return redirect("/helpdesk")
+        else:
+            flash("Invalid email or password.")
+            return redirect("/login")
 
     return render_template("login.html")
 
